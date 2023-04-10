5812eff35d4d222afb741f81d13c85c68a75a03d
Emerson Smith <emersonsmith@Emersons-MBP.attlocal.net>
2023-04-10 18:15:40 -0400

    not fixed, trying again

--- stripe/stripe.router.js
+++ stripe/stripe.router.js
@@ -4,9 +4,9 @@ const { validateAuth0 } = require("../middleware/auth0.middleware");
 
 const stripeRouter = express.Router();
 
+stripeRouter.post("/webhook", express.raw({ type: "application/json" }), webhook);
 stripeRouter.post("/create-checkout-session", validateAuth0, createCheckoutSession);
 stripeRouter.post("/create-portal-session", validateAuth0, createPortalSession);
 stripeRouter.get("/redirect", redirectURLToChromeURL);
-stripeRouter.post("/webhook", express.raw({ type: "application/json" }), webhook);
 
 module.exports = { stripeRouter };
